362e9de82eddb6e316c6a0a0e06ee28fd60faaa2
Giorgio Mazzei <40761205+giorgiomazzei79@users.noreply.github.com>
2023-01-13 14:59:44 +0100

    Initial Commit

---
+++ demo-app/demo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_appApp.swift
+//  demo-app
+//
+//  Created by Giorgio Mazzei on 13/01/23.
+//
+
+import SwiftUI
+
+@main
+struct demo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
